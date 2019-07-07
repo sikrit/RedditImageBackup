19817f558d420d5e71d6beb7094bc83b9cfb888b
Lukas Papik <papikdev@gmail.com>
2019-07-07 18:38:24 +0200

    just config

--- RedditGrabber.py
+++ RedditGrabber.py
@@ -19,46 +19,62 @@ from resources.handlers.common import Common
 from resources.save import Save
 from resources.db_interface import DBInterface
 
+
 class color:
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   END = '\033[0m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
 
 with open('./resources/config.json') as f:
-        config = json.load(f)
+    config = json.load(f)
 save = Save(os.getcwd(), False)
 logger = logging.getLogger(__name__)
 db = None
 
+
 def grabber(subR, base_dir, posts, sort):
     # Initialise Reddit
-    reddit = praw.Reddit(client_id = config["reddit"]["creds"]["client_id"],
-                        client_secret= config["reddit"]["creds"]["client_secret"],
-                        user_agent= config["reddit"]["creds"]["user_agent"])
+    reddit = praw.Reddit(client_id=config["reddit"]["creds"]["client_id"],
+                         client_secret=config["reddit"]["creds"]["client_secret"],
+                         user_agent=config["reddit"]["creds"]["user_agent"])
 
     if 'u/' in subR or '/u/' in subR:
-        if '/u/' in subR: subR = subR[3:]
-        elif 'u/'in subR: subR = subR[2:]
-        if sort == 'hot': submissions = reddit.redditor(subR).submissions.hot(limit = int(posts))
-        elif sort == 'new': submissions = reddit.redditor(subR).submissions.new(limit = int(posts))
-        elif sort =='top': submissions = reddit.redditor(subR).submissions.top(limit = int(posts))
+        if '/u/' in subR:
+            subR = subR[3:]
+        elif 'u/'in subR:
+            subR = subR[2:]
+        if sort == 'hot':
+            submissions = reddit.redditor(
+                subR).submissions.hot(limit=int(posts))
+        elif sort == 'new':
+            submissions = reddit.redditor(
+                subR).submissions.new(limit=int(posts))
+        elif sort == 'top':
+            submissions = reddit.redditor(
+                subR).submissions.top(limit=int(posts))
     else:
-        if sort == 'hot': submissions = reddit.subreddit(subR).hot(limit = int(posts))
-        elif sort == 'new': submissions = reddit.subreddit(subR).new(limit = int(posts))
-        elif sort == 'top': submissions = reddit.subreddit(subR).top(limit = int(posts))
+        if sort == 'hot':
+            submissions = reddit.subreddit(subR).hot(limit=int(posts))
+        elif sort == 'new':
+            submissions = reddit.subreddit(subR).new(limit=int(posts))
+        elif sort == 'top':
+            submissions = reddit.subreddit(subR).top(limit=int(posts))
 
     for submission in submissions:
         title = submission.title
         logger.debug("Submission url {}".format(submission.url))
-        #TODO find a better way to do this
+        # TODO find a better way to do this
         link = submission.url
         # if "youtube.com/watch" not in submission.url and "liveleak.com/view" not in submission.url:
         #     link = re.sub("\?(.)+", "", submission.url)
         if not (db.checkPost(submission.permalink.split("/")[4])) and not(submission.author in config["reddit"]["blacklist"]):
-            print_title = title.encode('utf-8')[:25] if len(title) > 25 else title.encode('utf-8')
-            logger.info("Post: {}... From: {} By: {}".format(print_title, subR, str(submission.author)))
+            print_title = title.encode(
+                'utf-8')[:25] if len(title) > 25 else title.encode('utf-8')
+            logger.info("Post: {}... From: {} By: {}".format(
+                print_title, subR, str(submission.author)))
             title = formatName(title)
-            
+
             # Selftext post
             if submission.is_self:
                 with open(os.path.join(save.get_dir(str(submission.author), str(submission.subreddit)), title + '.txt'), 'a+') as f:
@@ -66,35 +82,41 @@ def grabber(subR, base_dir, posts, sort):
 
             # Link to a jpg, png, gifv, gif, jpeg
             elif any(ext in link for ext in ['.jpg', '.png', '.gif', 'gifv', 'jpeg']) or 'i.reddituploads.com' in link:
-                Common(link, title, save.get_dir(str(submission.author), str(submission.subreddit)))
+                Common(link, title, save.get_dir(
+                    str(submission.author), str(submission.subreddit)))
 
             # Imgur
             elif 'imgur.com/' in link:
-                Imgur(link, title, save.get_dir(str(submission.author), str(submission.subreddit)))
+                Imgur(link, title, save.get_dir(
+                    str(submission.author), str(submission.subreddit)))
 
             # Giphy
             elif 'giphy.com/gifs' in link:
-                Giphy(link, title, save.get_dir(str(submission.author), str(submission.subreddit)))
+                Giphy(link, title, save.get_dir(
+                    str(submission.author), str(submission.subreddit)))
 
             # Tenor
             elif 'tenor.com/view' in link:
-                Tenor(link, title, save.get_dir(str(submission.author), str(submission.subreddit)))
+                Tenor(link, title, save.get_dir(
+                    str(submission.author), str(submission.subreddit)))
 
             # Flickr
             elif 'flickr.com/' in link:
                 logger.debug("No Flickr support")
                 with open(os.path.join(base_dir, 'error.txt'), 'a+') as logFile:
-                        logFile.write('Needs support: ' + link + '\n')
+                    logFile.write('Needs support: ' + link + '\n')
 
             # Reddit submission
             elif 'reddit.com/r/' in link:
                 with open(os.path.join(base_dir, 'error.txt'), 'a+') as logFile:
-                    logFile.write('Link to reddit ' + link + ' by ' + str(submission.author) + ' \n')
+                    logFile.write('Link to reddit ' + link +
+                                  ' by ' + str(submission.author) + ' \n')
                     logFile.close()
-            
+
             # All others are caught by youtube-dl, if still no match it's written to the log file
             else:
-                folder = save.get_dir(str(submission.author), str(submission.subreddit))
+                folder = save.get_dir(
+                    str(submission.author), str(submission.subreddit))
                 ydl_opts = {
                     'format': 'best',
                     'outtmpl': os.path.join(folder, '%(title)s-%(id)s.%(ext)s'),
@@ -107,25 +129,32 @@ def grabber(subR, base_dir, posts, sort):
                     logger.info("No matches: {}".format(link))
                     with open(os.path.join(base_dir, 'error.txt'), 'a+') as logFile:
                         logFile.write('No matches: ' + link + '\n')
-            
-            db.insertPost(submission.permalink, submission.title, submission.created, str(submission.author), submission.url)
+
+            db.insertPost(submission.permalink, submission.title, submission.created, str(
+                submission.author), submission.url)
+
 
 '''
 Removes special characters and shortens long
 filenames
 '''
+
+
 def formatName(title):
     title = re.sub('[?/|\\\:<>*"]', '', title)
-    if len(title) > 211: title = title[:210]
+    if len(title) > 211:
+        title = title[:210]
     return title
+
 
 def feeder(subR, posts, base_dir, sort):
     # reloads config file
     with open('./resources/config.json') as f:
         config = json.load(f)
-    
+
     logger.info("*****  {}  *****".format(subR))
     grabber(subR, base_dir, posts, sort)
+
 
 def main(args):
 
@@ -133,9 +162,9 @@ def main(args):
     filepath = None
 
     if args.subreddit:
-        if '.txt' in args.subreddit: 
+        if '.txt' in args.subreddit:
             filepath = args.subreddit
-        else: 
+        else:
             subR = args.subreddit
 
     # wait
@@ -157,11 +186,12 @@ def main(args):
             sys.exit()
     else:
         posts = 50
-    
+
     # output
     if args.output and args.subreddit:
         base_dir = os.path.abspath(args.output)
-        if not os.path.exists(base_dir): os.makedirs(base_dir)
+        if not os.path.exists(base_dir):
+            os.makedirs(base_dir)
     else:
         base_dir = os.getcwd()
 
@@ -176,20 +206,20 @@ def main(args):
     # blacklist
     if args.blacklist:
         config["reddit"]["blacklist"].append(args.blacklist)
-    
+
     # reddit api credentials
     if args.reddit_id:
         config["reddit"]["creds"]["client_id"] = args.reddit_id
     if args.reddit_secret:
         config["reddit"]["creds"]["client_secret"] = args.reddit_secret
-    
+
     with open('./resources/config.json', 'w') as f:
         json.dump(config, f)
 
     # by_author
     global save
     save = Save(base_dir, args.by_author)
-    
+
     # initialise database
     global db
     db = DBInterface(config["general"]["database_location"])
@@ -212,17 +242,26 @@ def main(args):
 
 if __name__ == '__main__':
     # Parses input
-    parser = argparse.ArgumentParser(description = "Archives submissions from Reddit")
-    parser.add_argument("subreddit", nargs = '?', help = "Enter a subreddit to backup or text file")
-    parser.add_argument("-w", "--wait", help = "Change wait time between subreddits in seconds")
-    parser.add_argument("-p", "--posts", help = "Number of posts to grab on each cycle")
-    parser.add_argument("-o", "--output", help = "Set base directory to start download")
-    parser.add_argument("--by_author", help = "Sort downloads by author, default by subreddit", action = "store_true")
-    parser.add_argument("--sort", help = "Sort submissions by 'hot', 'new' or 'top'")
-    parser.add_argument("-v", "--verbose", help = "Set verbose", action = "store_true")
-    parser.add_argument("--blacklist", help = "Avoid downloading a user, without /u/")
-    parser.add_argument('--reddit_id', help = 'Reddit client ID')
-    parser.add_argument('--reddit_secret', help = 'Reddit client secret')
+    parser = argparse.ArgumentParser(
+        description="Archives submissions from Reddit")
+    parser.add_argument("subreddit", nargs='?',
+                        help="Enter a subreddit to backup or text file")
+    parser.add_argument(
+        "-w", "--wait", help="Change wait time between subreddits in seconds")
+    parser.add_argument(
+        "-p", "--posts", help="Number of posts to grab on each cycle")
+    parser.add_argument(
+        "-o", "--output", help="Set base directory to start download")
+    parser.add_argument(
+        "--by_author", help="Sort downloads by author, default by subreddit", action="store_true")
+    parser.add_argument(
+        "--sort", help="Sort submissions by 'hot', 'new' or 'top'")
+    parser.add_argument("-v", "--verbose",
+                        help="Set verbose", action="store_true")
+    parser.add_argument(
+        "--blacklist", help="Avoid downloading a user, without /u/")
+    parser.add_argument('--reddit_id', help='Reddit client ID')
+    parser.add_argument('--reddit_secret', help='Reddit client secret')
 
     args = parser.parse_args()
 
@@ -236,18 +275,19 @@ if __name__ == '__main__':
                         datefmt='%m-%d %H:%M',
                         filename=config["general"]["log_file"],
                         filemode=filemode)
-    
+
     console = logging.StreamHandler()
-    if args.verbose and args.subreddit:  
+    if args.verbose and args.subreddit:
         console.setLevel(logging.DEBUG)
-        formatter = ColoredFormatter("[%(name)s][%(levelname)s] %(message)s (%(filename)s:%(lineno)d)")
+        formatter = ColoredFormatter(
+            "[%(name)s][%(levelname)s] %(message)s (%(filename)s:%(lineno)d)")
     else:
         console.setLevel(logging.INFO)
         formatter = logging.Formatter('%(message)s')
 
     console.setFormatter(formatter)
     logging.getLogger('').addHandler(console)
-    
+
     try:
         main(args)
     except exceptions.ResponseException as err:
--- resources/handlers/common.py
+++ resources/handlers/common.py
@@ -1,4 +1,4 @@
-import re 
+import re
 import os
 import time
 import json
@@ -6,6 +6,7 @@ from urllib.request import urlopen, Request, urlretrieve
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup as soup
 import logging
+
 
 class Common:
 
@@ -15,21 +16,22 @@ class Common:
         self.name = name
         self.direct = direct
         self.load_config()
-        
+
         self.save()
-    
+
     def load_config(self):
         with open('resources/config.json') as json_data_file:
             data = json.load(json_data_file)
         try:
             int(data["media_download"]["retries"])
             int(data["media_download"]["wait_time"])
-            
+
             self.retries = data["media_download"]["retries"]
             self.wait_time = data["media_download"]["wait_time"]
 
         except:
-            self.logger.error("TypeError: Media download retries or wait time is not an integer.")
+            self.logger.error(
+                "TypeError: Media download retries or wait time is not an integer.")
             self.retries = 5
             self.wait_time = 60
 
@@ -40,31 +42,33 @@ class Common:
         elif 'i.reddituploads.com' in self.link:
             ext = '.jpeg'
         else:
-            ext = '.' + re.search('jpg$|png$|gif$|jpeg$|mp4$', self.link).group()
-        self.direct = os.path.join(self.direct, self.format_name(self.name) + ext)
-        
+            ext = '.' + \
+                re.search('jpg$|png$|gif$|jpeg$|mp4$', self.link).group()
+        self.direct = os.path.join(
+            self.direct, self.format_name(self.name) + ext)
+
         self.logger.debug("Saving {} with extension {}".format(self.link, ext))
-        
+
         self.save_image()
 
-    def save_image(self, current_retry = 1):
+    def save_image(self, current_retry=1):
         try:
             urlretrieve(self.link, self.direct)
         except URLError:
             if self.retries > current_retry:
                 self.logger.warning("Retrying {}".format(self.link))
-                time.sleep(60)
+                time.sleep(5)
                 current_retry += 1
                 self.save_image(current_retry)
             else:
                 self.logger.error("Failed {}".format(self.link))
                 with open(os.path.join(os.getcwd(), 'error.txt'), 'a+') as logFile:
                     logFile.write('URLError: {}\n'.format(self.link))
-            
+
     def get_html(self):
         req = Request(
-            self.link, 
-            data=None, 
+            self.link,
+            data=None,
             headers={
                 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
             }
@@ -75,8 +79,9 @@ class Common:
         except HTTPError:
             page_html = None
         return page_html
- 
+
     def format_name(self, title):
         title = re.sub('[?/|\\\:<>*"]', '', title)
-        if len(title) > 190: title = title[:120]
+        if len(title) > 190:
+            title = title[:120]
         return title
